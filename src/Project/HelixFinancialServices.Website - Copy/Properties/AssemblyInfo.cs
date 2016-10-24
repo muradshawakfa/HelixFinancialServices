37faa7625a533f28ff6e060c3f5b4a8f3d221963
muradshawakfa <mshawakfa@tanasuk.com>
2016-10-24 11:49:08 +0300

    Added the project under new physical path

--- src/Project/HelixFinancialServices.Website - Copy/Properties/AssemblyInfo.cs
+++ src/Project/HelixFinancialServices.Website - Copy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelixFinancialServices")]
+[assembly: AssemblyTitle("HelixFinancialServices.Website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelixFinancialServices")]
+[assembly: AssemblyProduct("HelixFinancialServices.Website")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
